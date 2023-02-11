1a0fedeca6d1a1d4cc6b33d1fd1f5367874197cb
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-02-11 07:39:15 -0300

    verificando se o formulario existe, adicionado evento ao subemeter e previnindo a ação de carregamento da pagina ao clicar no botao

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const formulario = document.getElementById('formulario');
 const numeroA = document.getElementById('numero-1');
 const numeroB = document.getElementById('numero-2');
+
+if (formulario) {
+  formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
+}

2cae8e69a11f2a93cbc544f107c72f71b32412c7
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-02-11 08:03:32 -0300

    verificando se os inputs existem, e variavel com retorno booleano da validacao

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const formulario = document.getElementById('formulario');
-const numeroA = document.getElementById('numero-1');
-const numeroB = document.getElementById('numero-2');
+const numeroA = document.querySelector('#numero-1');
+const numeroB = document.querySelector('#numero-2');
 
 if (formulario) {
   formulario.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    if (numeroA && numeroB) {
+      const validacaoNumero = +numeroB.value > +numeroA.value ? true : false;
+      console.log(validacaoNumero);
+    }
   });
 }

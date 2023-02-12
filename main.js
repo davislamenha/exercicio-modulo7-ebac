adcb9e3739c4d62bdf534d875fedfea646b4857f
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-02-11 21:00:55 -0300

    mensagem adicionada

--- main.js
+++ main.js
@@ -8,7 +8,22 @@ if (formulario) {
 
     if (numeroA && numeroB) {
       const validacaoNumero = +numeroB.value > +numeroA.value ? true : false;
-      console.log(validacaoNumero);
+      const mensagem = document.querySelector('.mensagem');
+
+      if (validacaoNumero) {
+        mensagem.innerHTML = 'Enviado com sucesso!';
+        mensagem.classList.add('valido');
+        setTimeout(() => {
+          mensagem.classList.remove('valido');
+        }, 3000);
+      } else {
+        mensagem.innerHTML =
+          'Campo B precisa ser maior que o A, tente novamente!';
+        mensagem.classList.add('invalido');
+        setTimeout(() => {
+          mensagem.classList.remove('invalido');
+        }, 3000);
+      }
     }
   });
 }
